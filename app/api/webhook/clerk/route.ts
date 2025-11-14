766f5066149c92594c0022bcbe882315de5f6e85
Ajiboye <ajiboyemuyideen7@gmail.com>
2025-11-14 13:04:58 +0100

    jelo

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -71,7 +71,7 @@ export const POST = async (request: Request) => {
     try {
       // @ts-ignore
       await createCommunity(
-        // @ts-ignore
+       
         id,
         name,
         slug,
